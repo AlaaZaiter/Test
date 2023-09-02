2414ec001ce8b1f61a3d451b0ae929a2f3fcfd35
AlaaZaiter2 <zaiteralaa29@gmail.com>
2023-09-02 13:04:42 +0300

    Hosting challenge

---
+++ script.js
@@ -0,0 +1,13 @@
+// Array1 containing numbers using Array Literal
+const array1 = [1, 2, 3, 4, 5];
+
+// Array2 containing strings using Array Constructor
+const array2 = new Array('apple', 'banana', 'orange');
+
+// Array3 mixed with numbers and strings using Array.from()
+const array3 = Array.from([1, 'hello', 3, 'world', 5]);
+
+// Console.log all the arrays
+console.log(array1);
+console.log(array2);
+console.log(array3);
